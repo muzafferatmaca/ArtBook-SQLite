8cdef3bf2052531950a478d2e2f653879b6ef969
Muzaffer Atmaca <muzafferatmaca@gmail.com>
2022-06-02 13:21:37 +0300

    Initial commit

---
+++ app/src/main/java/com/muzafferatmaca/artbook/Art.kt
@@ -0,0 +1,7 @@
+package com.muzafferatmaca.artbook
+
+/**
+ * Created by Muzaffer Atmaca on 24.05.2022.
+ */
+class Art(val name : String,val id : Int) {
+}
---
+++ app/src/main/java/com/muzafferatmaca/artbook/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.muzafferatmaca.artbook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.muzafferatmaca.artbook.databinding.ActivityMainBinding
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var artList : ArrayList<Art>
+    private lateinit var artAdapter: ArtAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        artList = ArrayList<Art>()
+        artAdapter = ArtAdapter(artList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter=artAdapter
+
+
+
+
+
+        try {
+
+            val database = this.openOrCreateDatabase("Arts", MODE_PRIVATE,null)
+            val cursor = database.rawQuery("SELECT * FROM arts",null)
+            val nameIx = cursor.getColumnIndex("artname")
+            val idIx = cursor.getColumnIndex("id")
+
+            while (cursor.moveToNext()){
+
+                val name = cursor.getString(nameIx)
+                val id = cursor.getInt(idIx)
+                val art = Art(name,id)
+                artList.add(art)
+            }
+
+            artAdapter.notifyDataSetChanged()
+
+            cursor.close()
+
+        }catch (e:Exception){
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.art_menu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.addArtItem){
+            val intent = Intent(this@MainActivity,DetailActivity::class.java)
+            intent.putExtra("info","new")
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
